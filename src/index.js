763e64a0645fee6854d931041043203e4489f070
rowan-maynor <rowanmaynor@gmail.com>
2024-12-19 05:20:34 -0500

    Change todo list to be an empty array

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Project from "./project";
 import "./styles.css";
 
-const testJSON = '{"name": "test", "description": "test2"}';
+const testJSON = '{"name": "test"}';
 const testData = JSON.parse(testJSON);
 
 const test = new Project(testData);
+const test2 = new Project({name: "test2", toDoList: ["Test2", "Test3"]})
 
 console.log(test);
 
--- src/project.js
+++ src/project.js
@@ -1,8 +1,6 @@
 export default class Project {
     constructor(args) {
         this.name = `${args.name}`;
-        this.description = `${args.description}`
-        this.dueDate = `${args.dueDate}`
-        this.priority = `${args.priority}`
+        const toDoList = [];
     }
 }
