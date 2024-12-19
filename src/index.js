462089bd7c7927351c13057afce128f70199a6a0
rowan-maynor <rowanmaynor@gmail.com>
2024-12-19 06:34:44 -0500

    Working test with adding an entry to project

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const newProjectCard = document.querySelector("#new-project-card");
 
 newProjectCard.addEventListener("click", () => {
     const newProject = new Project({name: "test4"});
+    newProject.addEntry(["Make title", "Short description of the todo entry", "01/20/25", "High"]);
     projects.push(newProject);
     console.log(projects);
 });
