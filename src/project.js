5c3258459362da370bf5a2f76663cd2064aebfc1
rowan-maynor <rowanmaynor@gmail.com>
2024-12-19 07:32:37 -0500

    Remove createToDoEntry.js
    not actually needed, and conflicts with new function
    name for renderProjects

--- src/project.js
+++ src/project.js
@@ -1,5 +1,3 @@
-import createToDoEntry from "./toDoEntry";
-
 export default class Project {
     constructor(args) {
         this.name = `${args.name}`;
@@ -12,7 +10,7 @@ export default class Project {
     }
 
     addEntry = function(values) {
-        const newEntry = createToDoEntry(values[0], values[1], values[2], values[3]);
+        const newEntry = [values[0], values[1], values[2], values[3]];
         this.toDoList.push(newEntry);
     }
 }
--- src/toDoEntry.js
+++
@@ -1,3 +0,0 @@
-export default function createToDoEntry(name, description, dueDate, priority) {
-    return [name, description, dueDate, priority];
-}
