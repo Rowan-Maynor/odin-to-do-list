845764a89ee19c1dd69c13619eec1d437c08e823
rowan-maynor <rowanmaynor@gmail.com>
2024-12-21 07:39:30 -0500

    Create all elements for name submission

--- src/createNewProjectOverlay.js
+++ src/createNewProjectOverlay.js
@@ -1,6 +1,36 @@
 export default function createNewProjectOverlay() {
+    //create container div
     const overlayContainer = document.createElement("div");
     overlayContainer.id = "overlay-container";
     overlayContainer.classList = "flex-container";
+
+    //create input container
+    const inputContainer = document.createElement("div");
+    inputContainer.id = "input-container";
+
+    //create header for input
+    const inputHeader = document.createElement("p");
+    inputHeader.id = "input-header";
+    inputHeader.textContent = "Project Name"
+    inputContainer.append(inputHeader);
+
+    //create input for project name
+    const input = document.createElement("input");
+    input.id = "project-name-input"
+    input.type = "text"
+    inputContainer.append(input);
+
+    //create button for input submission
+    const submit = document.createElement("button");
+    submit.id = "submit-name-btn";
+    inputContainer.append(submit);
+
+    //event listener for button should be added on 
+    //index to have access to projects[]
+
+    //attach module to overlay container
+    overlayContainer.append(inputContainer);
+
+    //attach entire overlay to body
     document.body.append(overlayContainer);
 }
