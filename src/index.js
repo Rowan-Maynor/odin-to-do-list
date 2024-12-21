863bf0a4767605f45db5565cf02f4abea1a236e1
rowan-maynor <rowanmaynor@gmail.com>
2024-12-21 08:51:36 -0500

    Render projects on page load

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import createNewProjectOverlay from "./createNewProjectOverlay";
+import renderProjects from "./renderProjects";
 import "./styles.css";
 
 const newProjectCard = document.querySelector("#new-project-card");
+renderProjects();
 
 newProjectCard.addEventListener("click", () => {
     createNewProjectOverlay();
