dba6996a1663adbbd1d7471f4856683ebaad3c84
rowan-maynor <rowanmaynor@gmail.com>
2024-12-23 13:02:57 -0500

    Remove redundant code from project.js

--- src/project.js
+++ src/project.js
@@ -1,17 +1,9 @@
 export default class Project {
     constructor(args) {
         this.name = `${args.name}`;
-        this.toDoList = [];
-        if(args.toDoList){
-            for (const entry of args.toDoList){
-                this.addEntry(entry);
-            }
-        }
-    }
 
-    //title[0], description[1], dueDate[2], priority[3]
-    addEntry = function(values) {
-        const newEntry = [values[0], values[1], values[2], values[3]];
-        this.toDoList.push(newEntry);
+        //Entry format
+        //[title[0], description[1], dueDate[2], priority[3]]
+        this.toDoList = [];
     }
 }
